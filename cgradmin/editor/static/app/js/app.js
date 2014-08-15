547c68c07515b4e9172b22ffa1d2a7052b0e1419
Radu Ioan Fericean <radu.fericean@itsyscom.com>
2014-08-15 11:51:30 +0300

    created controllers and partial views

--- cgradmin/editor/static/app/js/app.js
+++ cgradmin/editor/static/app/js/app.js
@@ -3,14 +3,28 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('cgradminApp', [
-  'ngRoute',
-  'cgradminApp.filters',
-  'cgradminApp.services',
-  'cgradminApp.directives',
-  'cgradminApp.controllers'
+    'ngRoute',
+    'cgradminApp.filters',
+    'cgradminApp.services',
+    'cgradminApp.directives',
+    'cgradminApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/timings', {templateUrl: 'partials/timings.html', controller: 'TimingsCtrl'});
+        $routeProvider.when('/destinations', {templateUrl: 'partials/destinations.html', controller: 'DestinationsCtrl'});
+        $routeProvider.when('/rates', {templateUrl: 'partials/rates.html', controller: 'RatesCtrl'});
+        $routeProvider.when('/destinationrates', {templateUrl: 'partials/destinationrates.html', controller: 'DestinationRatesCtrl'});
+        $routeProvider.when('/ratingplans', {templateUrl: 'partials/ratingplans.html', controller: 'RatingPlansCtrl'});
+        $routeProvider.when('/ratingprofiles', {templateUrl: 'partials/ratingprofiles.html', controller: 'RatingProfilesCtrl'});
+        $routeProvider.when('/rpaliases', {templateUrl: 'partials/rpaliases.html', controller: 'RatingProfileAliasesCtrl'});
+        $routeProvider.when('/cataliases', {templateUrl: 'partials/cataliases.html', controller: 'CategoryAliasesCtrl'});
+        $routeProvider.when('/actions', {templateUrl: 'partials/actions.html', controller: 'ActionsCtrl'});
+        $routeProvider.when('/actionplans', {templateUrl: 'partials/actionplans.html', controller: 'ActionPlansCtrl'});
+        $routeProvider.when('/actiontriggers', {templateUrl: 'partials/actiontriggers.html', controller: 'ActionTriggersCtrl'});
+        $routeProvider.when('/sharedgroups', {templateUrl: 'partials/sharedgroups.html', controller: 'SharedGroupsCtrl'});
+        $routeProvider.when('/accountactions', {templateUrl: 'partials/accountactions.html', controller: 'AccountActionsCtrl'});
+        $routeProvider.when('/lcrrules', {templateUrl: 'partials/lcrrules.html', controller: 'LcrRulesCtrl'});
+        $routeProvider.when('/derivedcharges', {templateUrl: 'partials/derivedcharges.html', controller: 'DerivedChargesCtrl'});
+        $routeProvider.when('/cdrstats', {templateUrl: 'partials/cdrstats.html', controller: 'CdrStatsCtrl'});
+        $routeProvider.otherwise({redirectTo: '/timings'});
+    }]);
--- cgradmin/editor/static/app/js/controllers.js
+++ cgradmin/editor/static/app/js/controllers.js
@@ -3,12 +3,40 @@
 /* Controllers */
 
 angular.module('cgradminApp.controllers', [])
-    .controller('MyCtrl1', ['$scope', function($scope) {
+    .controller('TimingsCtrl', ['$scope', function($scope) {
     }])
-    .controller('MyCtrl2', ['$scope', '$http', function($scope, $http) {
+    .controller('DestinationsCtrl', ['$scope', '$http', function($scope, $http) {
         var url = '/call/CDRStatsV1.GetMetrics';
         var param = {StatsQueueId: 'CDRST1'};
         $http.post(url, param).success(function(data, status, headers, config) {
             $scope.status = data;
         });
+    }])
+    .controller('RatesCtrl', ['$scope', function($scope) {
+    }])
+    .controller('DestinationRatesCtrl', ['$scope', function($scope) {
+    }])
+    .controller('RatingPlansCtrl', ['$scope', function($scope) {
+    }])
+    .controller('RatingProfilesCtrl', ['$scope', function($scope) {
+    }])
+    .controller('RatingProfileAliasesCtrl', ['$scope', function($scope) {
+    }])
+    .controller('CategoryAliasesCtrl', ['$scope', function($scope) {
+    }])
+    .controller('LcrRulesCtrl', ['$scope', function($scope) {
+    }])
+    .controller('CdrStatsCtrl', ['$scope', function($scope) {
+    }])
+    .controller('ActionsCtrl', ['$scope', function($scope) {
+    }])
+    .controller('ActionPlansCtrl', ['$scope', function($scope) {
+    }])
+    .controller('ActionTriggersCtrl', ['$scope', function($scope) {
+    }])
+    .controller('AccountActionsCtrl', ['$scope', function($scope) {
+    }])
+    .controller('SharedGroupsCtrl', ['$scope', function($scope) {
+    }])
+    .controller('DerivedChargesCtrl', ['$scope', function($scope) {
     }]);
