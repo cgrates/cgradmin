ed7008f955429185968361f36d74a63c40699617
DanB <danb@cgrates.org>
2014-09-10 09:59:41 +0200

    JSONClient to avoid buffer fill

--- cgradmin/editor/json_client.py
+++ cgradmin/editor/json_client.py
@@ -2,34 +2,35 @@ import json, socket, itertools
 from threading import Lock
 
 class JSONClient(object):
-   def __init__(self, addr):
-      self.socket = socket.create_connection(addr)
-      self.id_counter = itertools.count()
-      self.BUFFER_SIZE = 4096
-
-   def __del__(self):
-      if hasattr(self, 'socket'):
-         self.socket.close()
-   def call(self, name, *params):
-      request = dict(id=next(self.id_counter),
-                     params=list(params),
-                     method=name)
-      self.socket.sendall(json.dumps(request).encode())
-      # This must loop if resp is bigger than 4K
-      resp = self.socket.recv(self.BUFFER_SIZE)
-      response = resp
-      while(len(resp) == self.BUFFER_SIZE):
-         resp = self.socket.recv(self.BUFFER_SIZE)
-         response += resp
-      response = json.loads(response.decode())
-      if response.get('id') != request.get('id'):
-         raise Exception("expected id=%s, received id=%s: %s"
-                         %(request.get('id'), response.get('id'),
-                           response.get('error')))
-      if response.get('error') is not None:
-         raise Exception(response.get('error'))
-      return response.get('result')
-
+    socket = None
+    def __init__(self, addr):
+        self.socket = socket.create_connection(addr)
+        self.id_counter = itertools.count()
+    def __del__(self):
+        if self.socket is not None:
+            self.socket.close()
+    def call(self, name, *params):
+        request = dict(id=next(self.id_counter),
+                    params=list(params),
+                    method=name)
+        self.socket.sendall(json.dumps(request).encode())
+        # This must loop if resp is bigger than 4K
+        #response = self.socket.recv(4096)
+        buffer = ''
+        data = True
+        while data:
+            data = self.socket.recv(4096)
+            buffer += data
+            if len(data) < 4096:
+                break
+        response = json.loads(buffer.decode())
+        if response.get('id') != request.get('id'):
+            raise Exception("expected id=%s, received id=%s: %s"
+                    %(request.get('id'), response.get('id'),
+                        response.get('error')))
+        if response.get('error') is not None:
+            raise Exception(response.get('error'))
+        return response.get('result')
 
 class CGRConnector(object):
     def __init__(self):
